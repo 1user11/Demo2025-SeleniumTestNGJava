f02ec95dcb0f3e36d8390adf2207d5b8e3d2cb62
ok <okepych@yahoo.com>
2025-10-26 16:48:14 -0500

    Base Test updated

--- src/test/java/com/websitename/base/BaseTest.java
+++ src/test/java/com/websitename/base/BaseTest.java
@@ -1,6 +1,6 @@
 package com.websitename.base;
 
-import io.qameta.allure.Allure;
+import com.websitename.utils.Utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,37 +15,8 @@ public abstract class BaseTest {
     protected WebDriver driver() {
         return driver;
     }
-    //    protected WebDriver driver;
-//
-//    @BeforeMethod
-//    public void setUp() {
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--disable-gpu");
-//        options.addArguments("--remote-allow-origins=*");
-//        options.addArguments("--window-size=1920,1080");
-//
-//
-////        ChromeOptions options = new ChromeOptions();
-////        String chromeOpts = System.getenv("CHROME_OPTIONS");
-////        if (chromeOpts != null) {
-////            options.addArguments(chromeOpts.split(";"));
-////        }
-//
-//        driver = new ChromeDriver(options);
-////        driver.get("https://askomdch.com/");
-//        driver.get("https://automationexercise.com/");
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
-//    }
 
-//    @AfterMethod
-//    public void tearDown() {
-//        driver.quit();
-//    }
-
-    protected WebDriver driver; // <- protected so subclasses can access
+    protected WebDriver driver;
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
@@ -56,16 +27,11 @@ public abstract class BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
     }
 
-//    @AfterMethod
-//    public void tearDown() {
-//        driver.quit();
-//    }
-
     @AfterMethod(alwaysRun = true)
     public void tearDown(ITestResult result) {
         try {
             if (!result.isSuccess() && driver != null) {
-                com.websitename.utils.Helpers.attachScreenshot(driver, "Failure: " + result.getName());
+                Utils.attachScreenshot(driver, "Failure: " + result.getName());
                 try {
                     io.qameta.allure.Allure.addAttachment(
                             "Page Source", "text/html", driver.getPageSource(), ".html"
