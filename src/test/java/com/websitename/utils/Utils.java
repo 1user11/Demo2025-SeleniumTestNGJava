c376136d3c85bed689072f9e047fbf5493f79d1f
ok <okepych@yahoo.com>
2025-10-26 16:51:33 -0500

    class updated

--- src/test/java/com/websitename/utils/Utils.java
+++ src/test/java/com/websitename/utils/Utils.java
@@ -6,19 +6,17 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.io.ByteArrayInputStream;
 
-public final class Helpers {
-    private Helpers(){}
+public class Utils {
 
-
-    public static void clearAndType(WebElement el, String text) { el.clear(); el.sendKeys(text); }
-    public static String text(WebElement el) { return el.getText(); }
-
-    public static void jsClick(WebDriver d, WebElement el) {
-        ((JavascriptExecutor) d).executeScript("arguments[0].click();", el);
+    public static void clearAndType(WebElement el, String text) {
+        el.clear();
+        el.sendKeys(text);
     }
-    public static void scrollIntoView(WebDriver d, WebElement el, boolean top) {
-        ((JavascriptExecutor) d).executeScript("arguments[0].scrollIntoView(arguments[1]);", el, top);
+
+    public static String text(WebElement el) {
+        return el.getText();
     }
+
     public static void hover(WebDriver d, WebElement el) {
         new Actions(d).moveToElement(el).perform();
     }
