02dbd39ad856dc3ee49e476ff61597353e80e48f
ok <okepych@yahoo.com>
2025-10-27 14:14:17 -0500

    added user data to test

--- src/test/java/com/websitename/tests/ui/auth/TC02_LoginTests_Positive.java
+++ src/test/java/com/websitename/tests/ui/auth/TC02_LoginTests_Positive.java
@@ -3,6 +3,7 @@ package com.websitename.tests.ui.auth;
 import com.websitename.base.BaseTest;
 import com.websitename.pages.auth.SignupLoginPage;
 import com.websitename.pages.home.HomePage;
+import com.websitename.utils.UserData;
 import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,17 +18,15 @@ public class TC02_LoginTests_Positive extends BaseTest {
 
     @Story("TC02: Login with valid credentials")
     @Severity(SeverityLevel.CRITICAL)
-    @Test(groups = {"ui","login","smoke"})
-        public void loginWithValidCredentials() {
-//            String email = System.getProperty("loginEmail");
-//            String password = System.getProperty("loginPassword");
-            HomePage home = new HomePage(driver).open();
+    @Test(groups = {"ui","auth","smoke"})
+    public void loginWithValidCredentials() {
+        UserData user = new UserData();
+        new HomePage(driver)
+                .open()
+                .goToSignupLogin();
+        new SignupLoginPage(driver)
+                .login(user.getValidEmail(), user.getValidPassword());
 
-            home.goToSignupLogin();
-            new SignupLoginPage(driver()).login("ememdems@hotmail.com", "8XbTY@zG@wYg2hg");
-
-            home = new HomePage(driver());
-
-            Assert.assertTrue(home.isLogoutVisible(), "Expected Logout link visible after login");
-        }
+        Assert.assertTrue(new HomePage(driver).isLogoutVisible(), "Logout button should be visible after successful login.");
+    }
 }
